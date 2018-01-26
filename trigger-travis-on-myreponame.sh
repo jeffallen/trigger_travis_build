626faa4941b80817c668b7f13b2771a2bbb10c86
Carla <iriberrig@gmail.com>
2018-01-26 12:18:23 +0100

    Update trigger-travis-on-myreponame.sh

--- trigger-travis-on-myreponame.sh
+++ trigger-travis-on-myreponame.sh
@@ -1,12 +1,24 @@
-TARGET_REPO=iriberri/travis-experiments
+TARGET_OWNER=iriberri
+TARGET_REPO=travis-experiments
 
 body='{ "request": { "branch":"master", "message": "Build triggered by $TRAVIS_REPO_SLUG" }}'
 
+# For Private Projects in travis-ci.com
+#
 curl -s -X POST \
      -H "Content-Type: application/json" \
      -H "Accept: application/json" \
      -H "Travis-API-Version: 3" \
      -H "Authorization: token $TRAVIS_TOKEN" \
      -d "$body" \
-     https://api.travis-ci.com/repo/$TARGET_REPO/requests
+     https://api.travis-ci.com/repo/$TARGET_OWNER%2F$TARGET_REPO/requests
      
+# For Public Projects in travis-ci.org
+# 
+# curl -s -X POST \
+#      -H "Content-Type: application/json" \
+#      -H "Accept: application/json" \
+#      -H "Travis-API-Version: 3" \
+#      -H "Authorization: token $TRAVIS_TOKEN" \
+#      -d "$body" \
+#      https://api.travis-ci.org/repo/$TARGET_OWNER%2F$TARGET_REPO/requests    
